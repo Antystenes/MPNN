1fa38e4470038bb3c1fb47328188765972446994
s372185 <pr20748@st.amu.edu.pl>
2018-06-15 12:57:30 +0200

    3rd hour

--- src/Lib.hs
+++ src/Lib.hs
@@ -6,7 +6,8 @@
            , AllowAmbiguousTypes
            , KindSignatures
            , TypeOperators
-           , RankNTypes #-}
+           , RankNTypes
+           , GeneralizedNewtypeDeriving #-}
 {-# OPTIONS_GHC -fplugin GHC.TypeLits.KnownNat.Solver #-}
 
 module Lib where
@@ -22,7 +23,21 @@ import Control.Monad
 import Numeric.AD
 
 newtype Vector (n :: Nat) = Vector { _vec :: VS.Vector Float }
-  deriving Show
+  deriving (Show, Floating)
+
+instance KnownNat n => Num (Vector n) where
+  (Vector v1) + (Vector v2) = Vector $ v1 + v2
+  (Vector v1) - (Vector v2) = Vector $ v1 - v2
+  (Vector v1) * (Vector v2) = Vector $ v1 * v2
+  abs                       = vectorMap abs
+  fromInteger               = vecFromList . (:[]) . fromInteger
+  negate                    = vectorMap negate
+  signum                    = vectorMap signum
+
+instance KnownNat n => Fractional (Vector n) where
+  (Vector v1) / (Vector v2) = Vector $ v1 / v2
+  fromRational              = vecFromList . (:[]) . fromRational
+
 
 natToInt :: forall n. KnownNat n => Int
 natToInt = fromIntegral . natVal $ Proxy @n
@@ -55,7 +70,7 @@ randomMatrix = matFromList <$> replicateM (w*h) (randomRIO (0.001, 0.1))
 data Network :: Nat -> Nat -> * where
   Layer :: forall m n. (KnownNat n, KnownNat m) =>
                        Matrix (n+1) m ->
-                       (forall a. Num a => a -> a) ->
+                       (forall a. Floating a => a -> a) ->
                        Network n m
 
 vectorMap :: (Float -> Float) -> Vector n -> Vector n
@@ -73,7 +88,7 @@ conductSignal :: forall n m. (KnownNat n, KnownNat m) =>
                                Vector m
 conductSignal (Layer mat f) = vectorMap f . mvMul mat . vectorCons 1
 
-randomLayer :: forall n m. (KnownNat n, KnownNat m) => (forall a. Num a => a -> a) -> IO (Network n m)
+randomLayer :: forall n m. (KnownNat n, KnownNat m) => (forall a. Floating a => a -> a) -> IO (Network n m)
 randomLayer f = (`Layer` f) <$> randomMatrix
 
 scaleMatrix :: (KnownNat n, KnownNat m) => Float -> Matrix m n -> Matrix m n
@@ -83,13 +98,14 @@ transposeMat :: (KnownNat n, KnownNat m) => Matrix n m -> Matrix m n
 transposeMat (Matrix mat) = Matrix $ LA.tr mat
 
 outerProduct :: (KnownNat n, KnownNat m) => Vector n -> Vector m -> Matrix n m
-outerProduct (Vector v1) (Vector v2) = Matrix $ v1 `outer` v2
+outerProduct (Vector v1) (Vector v2) = Matrix $ v2 `outer` v1
 
 vvMul :: (KnownNat n) => Vector n -> Vector n -> Vector n
 vvMul (Vector v1) (Vector v2) = Vector $ v1 * v2
 
 matrixSub :: (KnownNat m, KnownNat n) => Matrix n m -> Matrix n m -> Matrix n m
 matrixSub (Matrix m1) (Matrix m2) = Matrix $ m1 - m2
+
 
 useTrainingExample :: forall m n. (KnownNat n, KnownNat m) =>
                         (Vector m -> Vector m -> Vector m) ->
@@ -101,11 +117,52 @@ useTrainingExample lossGradient (Layer weights activation) (input, expected) = t
     trainedLayer = Layer newWeights activation
     newWeights :: Matrix (n+1) m
     newWeights = matrixSub weights $ scaleMatrix 0.01 weightGradient
-    output = conductSignal trainedLayer input
-    outputGradient = lossGradient output expected
-    activationDerivative = diff activation
-    weightGradient = outerProduct (vectorCons 1 input) .
-                     vvMul outputGradient .
-                     vectorMap activationDerivative .
-                     mvMul weights .
-                     vectorCons 1 $ input
+    consedInput = vectorCons 1 input
+    preActivationOutput = mvMul weights consedInput
+    output = vectorMap activation preActivationOutput
+    diffLoss = lossGradient expected output
+    diffActivation = vectorMap (diff activation) preActivationOutput
+    weightGradient = outerProduct consedInput (diffLoss * diffActivation)
+
+sigmoid input = 1/(1+exp(-input))
+                -- (1/) . (1+) . exp . negate
+
+epsilon :: Floating a => a
+epsilon = 0.0000001
+
+logLoss :: Floating a => [a] -> a
+logLoss [expected, output] = negate $ expected * log (output + epsilon) + (1 - expected) * log (1 - output + epsilon)
+
+gradLogLoss expected output = grad logLoss [expected, output] !! 1
+
+-- "mnist/train-set"
+
+getTrainSet :: IO [(Vector 784, Vector 10)]
+getTrainSet =
+  map (\(a,b) -> (vecFromList a, toOneHot b)) .
+  map (read @([Float],Int)) .
+  lines <$> readFile  "mnist/train-set"
+  where
+    toOneHot n = vecFromList $ map (\x -> if x == n then 1 else 0) [0..]
+
+eval network (input,expected) = do
+  putStrLn "EXPECTED:"
+  print expected
+  putStrLn "PREDICTED:"
+  print $ conductSignal network input
+
+trainTest = do
+  trainSet <- take 100 <$> getTrainSet
+  untrainedNetwork <- randomLayer sigmoid
+  let trainedNetwork = foldl (useTrainingExample gradLogLoss) untrainedNetwork trainSet
+      evalSet = take 10 trainSet
+  mapM_ (eval trainedNetwork) evalSet
+
+{-
+
+  diff (loss . activation . mvMul(weights, input))
+
+   => diff loss (activation . mvMul (weights,input)) * diff (activation . mvMul (weights,input))
+
+   => diff loss (output) * diff activation (mvMul (weights,input)) * diff mvMul(weights,input)
+-}
